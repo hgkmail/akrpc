c66a7837672f89eec60e3ed90a9596d9b40b33e8
kimhuang <hgkformap@gmail.com>
2026-01-22 15:18:06 +0800

    调整官网首页

--- akrpc-consumer/src/main/java/io/github/akrpc/consumer/AkrpcConsumerApplication.java
+++ akrpc-consumer/src/main/java/io/github/akrpc/consumer/AkrpcConsumerApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 
 @EnableRpcProvider
 @SpringBootApplication
-@EnableConfigurationProperties(value = {RpcConsumerProperties.class})
+@EnableConfigurationProperties(RpcConsumerProperties.class)
 public class AkrpcConsumerApplication {
 
     public static void main(String[] args) {
